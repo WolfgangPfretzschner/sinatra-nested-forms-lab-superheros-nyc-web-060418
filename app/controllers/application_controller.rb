523ea11db4ef7344cce9c54ba73a33e07a4d4830
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-06-27 23:47:58 -0400

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,24 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") }
 
-    get '/' do
-      erb :index
+  get '/' do
+    erb :index
+  end
+
+  post '/team' do
+    @team = Team.new(params[:team])
+
+    params[:team][:heroes].each do |details|
+      Hero.new(details)
     end
+    @heroes = Hero.all
+    # binding.pry
+
+    erb :team
+
+  end
 end
---
+++ app/models/team.rb
@@ -0,0 +1,20 @@
+class Team
+  attr_accessor :name, :motto
+
+  TEAM = []
+
+  def initialize(params)
+    @name = params[:name]
+    @motto = params[:motto]
+    TEAM << self
+  end
+
+  def self.all
+    TEAM
+  end
+  
+  def self.clear
+    all.clear
+  end
+
+end
